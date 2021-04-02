96d9355c2ae229577c19de9334c69ad09a1f1f5a
Abhi Jain <devabhixda@gmail.com>
2021-04-02 17:02:50 +0530

    MapBox integration

--- lib/const.dart
+++ lib/const.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 void navigateToPush(BuildContext context, Widget destination) {
   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => destination));
 }
+
+const mapBoxKey = "";
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:wasterage/Models/bin.dart';
+import 'package:wasterage/Services/api.dart';
+import 'package:wasterage/const.dart';
+import 'package:location/location.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,10 +14,67 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  MapboxMapController mapController;
+  LatLng myPos;
+  bool loading = true;
+
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  init() async {
+    LatLng temp = await acquireCurrentLocation();
+    setState(() {
+      myPos = temp; 
+      loading = false;
+    });
+  }
+
+  Future<LatLng> acquireCurrentLocation() async {
+    Location location = new Location();
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+    final locationData = await location.getLocation();
+    return LatLng(locationData.latitude, locationData.longitude);
+  }
+
+
+  void _onMapCreated(MapboxMapController controller) {
+    setState(() {
+      mapController = controller;      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
+    return loading ? Center(
+      child: SpinKitDoubleBounce(size: 20,
+      color: Colors.red,),
+    ) : Scaffold(
+      body: MapboxMap(
+        accessToken: mapBoxKey,
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: new CameraPosition(
+          target: myPos,
+          zoom: 11.0,
+        ),
+      ),
     );
   }
 }
